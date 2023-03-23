355784bba88e2978b0a78aa38010f6710cf1ee44
davi93fernandes <davi.a.fernandes@hotmail.com>
2023-03-23 20:59:56 -0300

    Adiciona conexao com api e validação dos dos inputs

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
+//ADICIONA EVENTO DE CARREGAMENTO NA JANELA E EVENTO DE CLICK NA FUNÇÃO CADASTRAR
 window.addEventListener("load", () => {
     const elementoBotao = document.getElementById("botaoCadastrar")
     elementoBotao.addEventListener("click", cadastrar)
 })
-
-
-
-function cadastrar() {
+//ELEMENTO INPUT EM MÃOS
+async function cadastrar() {
     const elementoTitulo = document.getElementById("tituloLivro")
     const elementoDescricao = document.getElementById("descricaoLivro")
-    titleInput = elementoTitulo.value
-    descriptionInput = elementoDescricao.value
-    console.log(titleInput)
-    console.log("clicou")
+    // VALOR DOS INPUTS 
+    title = elementoTitulo.value
+    description = elementoDescricao.value
+
+    if ((title === "" ) || (description === "")) {
+        document.getElementById("retornoErro").style.display = "inline-block"
+        document.getElementById("retornoErro").style.backgroundColor = "#ac6363"
+        document.getElementById("retornoErro").innerText = "FALTA ALGUMA INFORMAÇÃO"
+
+    } else{
+        //ATRIBUI URL Á UMA VARIAVEL
+        // TAMBÉM OS DADOS PARA API
+        const url = "https://target-api-simples.cyclic.app/livros"
+        const payload = {
+            title: title,
+            description: description
+        }
+
+        //ATRIBUI O RETORNO DA API A UMA VARIAVEL
+        const retorno = await dadosParaAPI(url, payload)
+        // console.log(retorno)
+
+        //ALTERA CSS DE UM OBJETO HTML(TAG DIV)
+        const elementoResposta = document.getElementById("retornoSucesso")
+        elementoResposta.style.display = "Block"
+        elementoResposta.innerHTML = await retorno.json()
+        document.getElementById("retornoErro").style.display = "none"
+    }
 }
+    //CRIA FUNÇÃO PARA CONFIGURAÇÃO E SELEÇÃO DE OBJETOS (CHAVE : VAOR) PARA API
+    async function dadosParaAPI(url, payload) {
+        const resposta = await fetch(url,
+            {
+                method: "POST",
+                headers: { "content-type": "application/json" },
+                body: JSON.stringify(payload)
+            })
+        return resposta
+
+    }
